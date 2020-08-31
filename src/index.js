0bf5d00757e40390956d87c69f02c4ed9f3d19ae
Dennis Rasmussen <mail@dennisrasmussen.dk>
2020-08-31 11:42:23 +0200

    Clone of discordjs.guide with minor changes

---
+++ src/index.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const Discord = require("discord.js");
+
+const prefix = process.env.COMMAND_PREFIX;
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+
+const commandFolder = path.resolve(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandFolder).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(path.resolve(commandFolder, file));
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+console.log("token", process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName) || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+  if (!client.cooldowns.has(command.name)) {
+    client.cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = client.cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  if (command.guildOnly && message.channel.type === "dm") {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+    return message.channel.send(reply);
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
